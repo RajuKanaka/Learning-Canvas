6be2c69e96d24609439cc5782a3a6241533b4efb
Kanaka Raju <kanaka@mudrantar.com>
2023-04-10 10:21:50 +0530

    styling(app): added header and styling refactoring

--- js/index.js
+++ js/index.js
@@ -4,12 +4,14 @@ import {
   drawRandomShape,
   drawTrapezoid,
 } from './basic-canvas/index.js';
-window.onload = function () {
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d');
-  //  getContext: returns an object that provides methods and properties for drawing on the canvas.
-  initailizeCanvas(ctx);
-  basicRect(ctx);
-  drawRandomShape(ctx);
-  drawTrapezoid(ctx, 750, 50, 150);
-};
+
+const canvas = document.getElementById('basic_canvas');
+const ctx = canvas.getContext('2d');
+//  getContext: returns an object that provides methods and properties for drawing on the canvas.
+if (!ctx) {
+  throw new Error('No 2D context found!');
+}
+initailizeCanvas(ctx);
+basicRect(ctx);
+drawRandomShape(ctx);
+drawTrapezoid(ctx, 750, 50, 150);
